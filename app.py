3dfbea65e4cc9aaf2d6813d299c7d64af299d33d
Logan Gall <141678252+logan-gall@users.noreply.github.com>
2023-11-18 13:36:42 -0600

    New Fixes!

--- app.py
+++ app.py
@@ -8,7 +8,6 @@
 import pandas as pd
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_squared_error
 
